d71cc9f8fa36e2f690799d509c2ec0cc9d432626
ms7awesome <ms7awesome@gmail.com>
2021-10-24 22:44:29 +0900

    첫번째 커밋 amend teset / cherry pick test
    # Conflicts:
    #	app/src/main/java/com/mscorp/gitplayground/MainActivity.kt

--- app/src/main/java/com/mscorp/gitplayground/MainActivity.kt
+++ app/src/main/java/com/mscorp/gitplayground/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // ms01 first commit
+        // first commit 
     }
 }
